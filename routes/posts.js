b39c130881d4474ee2b036cdfde120e439c1a47a
sinazay123 <sinazay123@gmail.com>
2021-06-24 20:41:20 -0500

    Implemented handler for get requests

--- routes/posts.js
+++ routes/posts.js
@@ -1,16 +1,49 @@
 const posts = require('../posts');
 
-function postRoutes (app, options, done) {
-    
-    app.get('/posts', (req, res) => {
+// Post Schema
+const Post = {
+    type: 'object',
+    properties: {
+        id: { type: 'string'},
+        content: { type: 'string'},
+        author: { type: 'string'}
+    }
+}
+// Options for getting all posts
+const getPostsOpts = {
+    schema: {
+        response: {
+            200: {
+                type: 'array',
+                posts: Post,
+                },
+            },
+        },
+    handler: function (req, res) {
         res.send(posts)
-    })
+    },
+}
+
+
+const getPostOpts = {
+    schema: {
+        response: {
+            200: Post
+        },
+    },
     
-    app.get('/posts/:id', (req, res) => {
+    handler: function (req, res) {
         const { id } = req.params
         const post = posts.find((post)=> post.id === id)
         res.send(post)
-    })
+    },
+}
+
+function postRoutes (app, options, done) {
+    
+    app.get('/posts', getPostsOpts)
+    
+    app.get('/posts/:id', getPostOpts)
 
     done()
 }
